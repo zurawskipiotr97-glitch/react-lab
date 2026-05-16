bcf8dc8a5479c337658592df8f202cb11d412fa9
zurawskipiotr97-glitch <zurawskipiotr97@gmail.com>
2026-05-16 14:27:54 +0200

    adding features

--- src/meetings/MeetingsList.js
+++ src/meetings/MeetingsList.js
@@ -1,10 +1,28 @@
+import {useState} from "react";
+
 export default function MeetingsList({meetings}) {
+    const [contestants, setContestants] = useState([])
+
+    function handleNewContestatn() {
+
+    }
+
+    function handleDeleteContestant() {
+
+    }
+
+    function handleDeleteMeeting() {
+
+    }
+
     return (
         <table>
             <thead>
             <tr>
                 <th>Nazwa spotkania</th>
                 <th>Opis</th>
+                <th>Uczestnicy</th>
+                <th></th>
             </tr>
             </thead>
             <tbody>
@@ -12,9 +30,30 @@ export default function MeetingsList({meetings}) {
                 meetings.map((meeting, index) => <tr key={index}>
                     <td>{meeting.title}</td>
                     <td>{meeting.description}</td>
-                </tr>)
-            }
-            </tbody>
-        </table>
+                    <td>{meeting.contestants}</td>
+                    <td float-left>
+                        {<div className="float-right">
+                            {
+                               true
+                            //        dopisz jakiś sensowny warunek
+
+                            ? <button className="button button-outline">Zapisz się</button>
+                            : <button className="button button-outline">Wypisz się</button>
+                            }
+
+                            {contestants.length === 0 && (
+                                <button className="button" style={{ marginLeft: '5px' }}>
+                                    Usuń puste spotkanie
+                                </button>
+                            )}
+                        </div>
+                            }
+
+                        </td>
+                        {/*    opcje do wyboru, zapisz, wypisz usuń puste*/}
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
     );
 }
--- src/meetings/MeetingsPage.js
+++ src/meetings/MeetingsPage.js
@@ -5,16 +5,56 @@ import MeetingsList from "./MeetingsList";
 export default function MeetingsPage() {
     const [meetings, setMeetings] = useState([]);
 
+    const [addNewClick, setAddNewClick] = useState(false)
+
     function handleNewMeeting(meeting) {
         const nextMeetings = [...meetings, meeting];
         setMeetings(nextMeetings);
     }
 
+    function handleClick() {
+        setAddNewClick(true)
+
+    }
+
     return (
         <div>
-            <h2>Zajęcia ({meetings.length})</h2>
-            <NewMeetingForm onSubmit={(meeting) => handleNewMeeting(meeting)}/>
-            <MeetingsList meetings={meetings}/>
+            {
+                addNewClick
+                    ?   <div>
+                        {/*<h2>Dodaj nowe spotkanie</h2>*/}
+                        <NewMeetingForm onSubmit={(meeting) => handleNewMeeting(meeting)}/>
+
+                        {
+                            meetings.length===0
+                                ?   <p>Brak zaplanowanych spotkań</p>
+                                :   <>
+                                    <h3>Zaplanowane spotkania ({meetings.length})</h3>
+                                    <MeetingsList meetings={meetings}/>
+
+                                </>
+                            }
+                    </div>
+                    :   <div>
+                        <button onClick={() => handleClick()}>
+                            Dodaj nowe spotkanie
+                        </button>
+                        {
+                            meetings.length===0
+                                ?   <p>Brak zaplanowanych spotkań</p>
+                                :   <>
+                                    <h3>Zaplanowane spotkania ({meetings.length})</h3>
+                                    <MeetingsList meetings={meetings}/>
+
+                                </>
+                        }
+
+                    </div>
+
+            }
+
         </div>
+
+
     )
 }
