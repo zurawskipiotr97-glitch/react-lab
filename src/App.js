605f1daa729239247c3a2d0f55acf26e5a87b17d
zurawskipiotr97-glitch <zurawskipiotr97@gmail.com>
2026-05-16 10:39:02 +0200

    Reactive email

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'zurawskipiotr97@gmail.com'
+    const [email, setEmail] =
+        useState('zurawskipiotr97@gmail.com');
+
+    let message;
+    if (email.length < 10) {
+        message = <div>Ale masz małego e-maila!</div>;
+    } else if (email.length < 20) {
+        message = <div>Twój adres e-mail jest w sam raz.</div>;
+    } else {
+        message = <div>Nie zapychaj serwerów, skróć!.</div>;
+    }
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
             <h2>Twój email to {email}</h2>
-            <input type="text" onChange={handleChange}/>
+            <p>{message}</p>
+            <input type="text" value={email} onChange={handleChange}/>
         </div>
     );
-
 }
-
 
 export default App;
