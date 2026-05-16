e7de0088cb3aebbbd8c6f006e9ff3f62bf8a604f
zurawskipiotr97-glitch <zurawskipiotr97@gmail.com>
2026-05-16 14:52:51 +0200

    adding features

--- src/meetings/MeetingsList.js
+++ src/meetings/MeetingsList.js
@@ -2,9 +2,11 @@ import {useState} from "react";
 
 export default function MeetingsList({meetings}) {
     const [contestants, setContestants] = useState([])
+    // const [contestantName, setContestantNmae]
 
-    function handleNewContestatn() {
-
+    function handleNewContestant(contestant) {
+        const nextContestants = [...contestants, contestant];
+        setContestants(nextContestants)
     }
 
     function handleDeleteContestant() {
@@ -30,30 +32,44 @@ export default function MeetingsList({meetings}) {
                 meetings.map((meeting, index) => <tr key={index}>
                     <td>{meeting.title}</td>
                     <td>{meeting.description}</td>
-                    <td>{meeting.contestants}</td>
+                        <td>
+
+                            {contestants.length > 0 ? (
+                                <ul style={{ margin: 0, paddingLeft: "20px" }}>
+                                    {contestants.map((contestant, index) => (
+                                        <li key={index}>{contestant}</li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <em style={{ color: "gray" }}>Brak uczestników</em>
+                            )}
+                        </td>
+
                     <td float-left>
                         {<div className="float-right">
                             {
                                true
                             //        dopisz jakiś sensowny warunek
 
-                            ? <button className="button button-outline">Zapisz się</button>
-                            : <button className="button button-outline">Wypisz się</button>
+                                    ? <button className="button button-outline" onClick={handleNewContestant}>Zapisz się</button>
+                                    : <button className="button button-outline">Wypisz się</button>
                             }
 
-                            {contestants.length === 0 && (
-                                <button className="button" style={{ marginLeft: '5px' }}>
-                                    Usuń puste spotkanie
-                                </button>
-                            )}
+                            {
+                                contestants.length === 0 && (
+                                    <button className="button" style={{ marginLeft: '5px' }}>
+                                        Usuń puste spotkanie
+                                    </button>
+                                )
+                            }
                         </div>
-                            }
-
-                        </td>
-                        {/*    opcje do wyboru, zapisz, wypisz usuń puste*/}
-                            </tr>)
                         }
-                    </tbody>
-                </table>
+
+                    </td>
+                </tr>
+                )
+            }
+            </tbody>
+        </table>
     );
 }
